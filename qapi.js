1220edf54e380b7c05aaf70086b1771648be5b2a
Roman BAD <57302328+romanoz33@users.noreply.github.com>
2021-03-16 13:07:53 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60508313465b2a001e0eddd9",
+				"60508313465b2a001e0edddb"
+			]
+		},
+		"60508313465b2a001e0eddd9": {
+			"id": "60508313465b2a001e0eddd9",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60508313465b2a001e0edddb": {
+			"id": "60508313465b2a001e0edddb",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
